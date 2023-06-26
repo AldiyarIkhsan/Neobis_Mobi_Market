97efa88a48b562985df4a40b86b8e502290d9bc2
AldiyarIkhsan <usoul31@gmail.com>
2023-06-26 21:48:09 +0500

    made some fixes in url of product

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import ProductRetrieveUpdateDestroyView, ProductLikeAPIView, ProductUnlikeAPIView
+from .views import ProductListCreateView
 
 urlpatterns = [
-    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
-    path('products/<int:product_id>/like/', ProductLikeAPIView.as_view(), name='product-likes'),
-    path('products/<int:product_id>/unlike/', ProductUnlikeAPIView.as_view(), name='product-unlike'),
+    path('products/<int:pk>/', ProductListCreateView.as_view(), name='product-detail'),
 ]
